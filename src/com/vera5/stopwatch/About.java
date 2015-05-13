00b990d53ddae605d4644980a14580e825c3875c
Nick Kolev <nickkolev@yahoo.com>
2015-05-13 09:05:10 +0200

    About in WebView * Refactored 'About' from View to WebView

--- src/com/vera5/stopwatch/About.java
+++ src/com/vera5/stopwatch/About.java
@@ -2,13 +2,18 @@ package com.vera5.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class About extends Activity {
+
+  private WebView myWebView;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
+		myWebView = (WebView) findViewById(R.id.about);
+		myWebView.loadUrl("file:///android_asset/about.htm");
 	}
 
 }
